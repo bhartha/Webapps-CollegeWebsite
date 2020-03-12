0d86cab066cbfba75419dfe8bb48657662931c52
Bharath Kumar <bhartha007@outlook.com>
2020-03-12 10:00:00 +0530

    Add project files.

---
+++ CollegeWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CollegeWebsite.Startup))]
+namespace CollegeWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
